37e284ee0efda9a9c964b2c79589e4de73f4ec07
Risang Radityatama <161427850+rxceed@users.noreply.github.com>
2025-09-16 21:31:42 +0700

    Create soal_2.c

---
+++ P2/soal_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  char kalimat;
+  for(int = 0; i < 5)
+    {
+      scanf("%d", kalimat[i]);
+    }
+    for(int = 0; i < 5)
+    {
+      printf("%d", kalimat[i]);
+    }
+}
