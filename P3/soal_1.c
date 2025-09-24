c3e588a3caf747e8f3dfabc04fdd4584c790e244
Risang Radityatama <161427850+rxceed@users.noreply.github.com>
2025-09-24 20:53:19 +0700

    Create soal_1.c

---
+++ P3/soal_1.c
@@ -0,0 +1,28 @@
+#include <stdio>
+
+int f(int x, int order, int n)
+{
+  int result;
+  //hitung hitung hitung
+  //kode untuk menghitung
+  //hitung hitung hitung
+  if(order != 1)
+  {
+    //rekursi.......
+    return result;
+  }
+  else
+  {
+    return result;
+  }
+}
+
+int main()
+{
+  //Bentuk persamaan polinomial: nx + nx^2 + nx^3 + ....... + nx^order
+  //Misal: x = 2, order = 3, n = 2, maka hasil y = 28
+  int x, order, n;
+  y = f(x, order, n);
+  printf("%d", y);
+  return 0;
+}
